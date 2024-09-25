3cde29d08b3523ca76627ca6e43632f29e56853f
Karim Biscaiburo <kbiscaiburo@gmail.com>
2024-09-25 18:07:49 -0300

    Agregando el FrontPage y las tecnologias

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				"sans": ["Libre Caslon Text", "sans-serif"]
+			},
+		},
+		colors: {
+			"nav_background": "rgba(0, 0, 0, 0.06)",
+			"black": "#0B0B0B",
+			"dark-gray": "#1C1C1D",
+			"white": "#DDDDDD",
+			"cian": "#09aa8c",
+			"darken-cian": "#072621"
+		},
 	},
 	plugins: [],
 }
